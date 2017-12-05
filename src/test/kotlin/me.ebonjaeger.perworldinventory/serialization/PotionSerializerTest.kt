93890b48dd6209abe73b2971bb32f593b013f868
Evan Maddock <maddock.evan@vivaldi.net>
2017-12-05 18:48:30 -0500

    serialization: Create serializer for potion effects
    The tests for this are currently broken and I have yet to figure out a way around it. The way potion effect types work seems very silly to me.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

---
+++ src/test/kotlin/me.ebonjaeger.perworldinventory/serialization/PotionSerializerTest.kt
@@ -0,0 +1,41 @@
+package me.ebonjaeger.perworldinventory.serialization
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.junit.Test
+
+class PotionSerializerTest
+{
+
+    @Test
+    fun verifySerializedPotions()
+    {
+        // given
+        val effects = mutableListOf<PotionEffect>()
+
+        val effect1 = PotionEffect(PotionEffectType.ABSORPTION, 5, 2, true, false)
+        val effect2 = PotionEffect(PotionEffectType.GLOWING, 27, 1)
+
+        effects.add(effect1)
+        effects.add(effect2)
+
+        // when
+        val json = PotionSerializer.serialize(effects)
+
+        // then
+        val result = PotionSerializer.deserialize(json)
+        assertHasSameProperties(result.first(), effect1)
+        assertHasSameProperties(result.last(), effect2)
+    }
+
+    private fun assertHasSameProperties(given: PotionEffect, expected: PotionEffect)
+    {
+        assertThat(given.type, equalTo(expected.type))
+        assertThat(given.duration, equalTo(expected.duration))
+        assertThat(given.amplifier, equalTo(expected.amplifier))
+        assertThat(given.isAmbient, equalTo(expected.isAmbient))
+        assertThat(given.hasParticles(), equalTo(expected.hasParticles()))
+    }
+}
