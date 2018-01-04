0fc2be4f3d687cb83a971b1047ceebd9679c327f
Evan Maddock <maddock.evan@vivaldi.net>
2018-01-04 15:05:09 -0500

    Add a collection of default values.
    This will be used later instead of the default files nonsense that the old version had.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

---
+++ src/main/kotlin/me/ebonjaeger/perworldinventory/PlayerStats.kt
@@ -0,0 +1,58 @@
+package me.ebonjaeger.perworldinventory
+
+/**
+ * A collection of default stats for a Player.
+ */
+object PlayerStats
+{
+
+    /**
+     * Default health value.
+     */
+    val HEALTH = 20
+
+    /**
+     * Default experience value.
+     */
+    val EXPERIENCE = 0F
+
+    /**
+     * Default level value.
+     */
+    val LEVEL = 0
+
+    /**
+     * Default food level value.
+     */
+    val FOOD_LEVEL = 20
+
+    /**
+     * Default exhaustion value.
+     */
+    val EXHAUSTION = 0.0F
+
+    /**
+     * Default saturation value.
+     */
+    val SATURATION = 5F
+
+    /**
+     * Default fall distance value.
+     */
+    val FALL_DISTANCE = 0F
+
+    /**
+     * Default fire ticks value.
+     */
+    val FIRE_TICKS = 0
+
+    /**
+     * Default remaining air value.
+     */
+    val REMAINING_AIR = 300
+
+    /**
+     * Default maximum air value.
+     */
+    val MAXIMUM_AIR = 300
+}
