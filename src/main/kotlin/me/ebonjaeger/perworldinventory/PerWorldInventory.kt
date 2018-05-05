04d0dd1f66cb2f037f04cffda13ff007e8716335
Evan Maddock <maddock.evan@vivaldi.net>
2018-05-05 00:45:40 -0400

    Implement death listener to prevent inventory duplication
    Fixes #37
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/kotlin/me/ebonjaeger/perworldinventory/PerWorldInventory.kt
+++ src/main/kotlin/me/ebonjaeger/perworldinventory/PerWorldInventory.kt
@@ -130,6 +130,7 @@ class PerWorldInventory : JavaPlugin
         server.pluginManager.registerEvents(injector.getSingleton(PlayerQuitListener::class), this)
         server.pluginManager.registerEvents(injector.getSingleton(PlayerTeleportListener::class), this)
         server.pluginManager.registerEvents(injector.getSingleton(EntityPortalEventListener::class), this)
+        server.pluginManager.registerEvents(injector.getSingleton(PlayerDeathListener::class), this)
 
         // The PlayerSpawnLocationEvent is only fired in Spigot
         // As of version 1.9.2
---
+++ src/main/kotlin/me/ebonjaeger/perworldinventory/listener/player/PlayerDeathListener.kt
@@ -0,0 +1,46 @@
+package me.ebonjaeger.perworldinventory.listener.player
+
+import me.ebonjaeger.perworldinventory.GroupManager
+import me.ebonjaeger.perworldinventory.data.PlayerDefaults
+import me.ebonjaeger.perworldinventory.data.ProfileManager
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import javax.inject.Inject
+
+/**
+ * Listens for [PlayerDeathEvent]s to prevent inventory
+ * duplication when the player re-enters the world.
+ */
+class PlayerDeathListener @Inject constructor(private val groupManager: GroupManager,
+                                              private val profileManager: ProfileManager) : Listener
+{
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onPlayerDeath(event: PlayerDeathEvent)
+    {
+        val player = event.entity
+        val group = groupManager.getGroupFromWorld(player.location.world.name)
+
+        if (!event.keepInventory)
+        {
+            player.inventory.clear()
+        }
+
+        if (!event.keepLevel)
+        {
+            player.totalExperience = event.newExp
+            player.level = event.newLevel
+        }
+
+        player.foodLevel = PlayerDefaults.FOOD_LEVEL
+        player.saturation = PlayerDefaults.SATURATION
+        player.exhaustion = PlayerDefaults.EXHAUSTION
+        player.fallDistance = PlayerDefaults.FALL_DISTANCE
+        player.fireTicks = PlayerDefaults.FIRE_TICKS
+        player.activePotionEffects.forEach { player.removePotionEffect(it.type) }
+
+        profileManager.addPlayerProfile(player, group, player.gameMode)
+    }
+}
