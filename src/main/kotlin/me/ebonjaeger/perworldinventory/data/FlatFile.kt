8fffe4f0860daeb4789d050ef4ea052d7deae84d
Evan Maddock <maddock.evan@vivaldi.net>
2018-06-18 22:36:30 -0400

    Try to catch the right exception when a file already exists
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/kotlin/me/ebonjaeger/perworldinventory/data/FlatFile.kt
+++ src/main/kotlin/me/ebonjaeger/perworldinventory/data/FlatFile.kt
@@ -15,6 +15,7 @@ import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 import java.io.IOException
+import java.nio.file.FileAlreadyExistsException
 import java.nio.file.Files
 import javax.inject.Inject
 
