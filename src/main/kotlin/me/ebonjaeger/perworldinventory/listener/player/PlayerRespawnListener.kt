a8d4a5735ac902d0c6140b1d23ac587b4fa9f972
Evan Maddock <maddock.evan@vivaldi.net>
2019-04-23 20:02:24 -0400

    Update nullability handling in PlayerRespawnListener
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/kotlin/me/ebonjaeger/perworldinventory/listener/player/PlayerRespawnListener.kt
+++ src/main/kotlin/me/ebonjaeger/perworldinventory/listener/player/PlayerRespawnListener.kt
@@ -1,5 +1,6 @@
 package me.ebonjaeger.perworldinventory.listener.player
 
+import me.ebonjaeger.perworldinventory.ConsoleLogger
 import me.ebonjaeger.perworldinventory.GroupManager
 import me.ebonjaeger.perworldinventory.configuration.PluginSettings
 import me.ebonjaeger.perworldinventory.configuration.Settings
@@ -25,7 +26,14 @@ class PlayerRespawnListener @Inject constructor(private val groupManager: GroupM
         val group = groupManager.getGroupFromWorld(event.player.location.world!!.name) // The server will never provide a null world in a Location
         val respawnWorld = group.respawnWorld
         if (respawnWorld != null && group.containsWorld(respawnWorld)) {
-            event.respawnLocation = Bukkit.getWorld(respawnWorld).spawnLocation
+            val world = Bukkit.getWorld(respawnWorld)
+
+            if (world == null) {
+                ConsoleLogger.warning("Unable to set respawn location: World '$respawnWorld' doesn't exist!")
+                return
+            }
+
+            event.respawnLocation = world.spawnLocation
         }
     }
 }
